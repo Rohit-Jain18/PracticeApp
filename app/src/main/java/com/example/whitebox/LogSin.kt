c46e380512f46902b56ad9617de9e7914a541d99
Rohit-Jain18 <jainrohit.official@gmail.com>
2024-05-09 23:29:05 +0530

    day may 09 updated Learning

--- app/src/main/java/com/example/whitebox/LogSin.kt
+++ app/src/main/java/com/example/whitebox/LogSin.kt
@@ -1,28 +1,35 @@
 package com.example.whitebox
 
-import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-
-
-class SplashScreen : AppCompatActivity() {
+class LogSin : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_splash_screen)
+        setContentView(R.layout.activity_log_sin)
+        val login = findViewById<Button>(R.id.log)
+        login.setOnClickListener{
+      val intent = Intent(this,MainActivity::class.java)
+      startActivity(intent)
+        }
 
-        Handler().postDelayed({
-                              val intent= Intent(this , LogSin::class.java)
-                              startActivity(intent)
-            finish()
 
-        },1000)
+
+
+
+
+
+
+
+
+
+
 
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
